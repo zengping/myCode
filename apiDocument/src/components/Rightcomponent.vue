7273c8fc913ad401393fd38f3f6cddb48ffd58c6
JOHN ZEN <JOHN ZEN>
2017-03-27 16:57:32 +0800

    modify

--- apiDocument/src/components/Rightcomponent.vue
+++ apiDocument/src/components/Rightcomponent.vue
@@ -1,3 +1,14 @@
+<style lang="">
+  h5 > span {
+    float: right;
+  }
+  .testBtn {
+    float: left;
+    display: block;
+    width: 100%;
+    margin: 50px auto;
+  }
+</style>
 <template>
   <div class="right" v-if="info.name">
       <div class="col-lg-9">
@@ -5,99 +16,118 @@
             <h2>
                 {{info.name}}
             </h2>
-            <h5>作者：{{info.author}}，版本：{{info.version}}</h5>
+            <h5>作者：{{info.author}}，版本：{{info.version}} <span class="btn btn-default" v-on:click="apiTest">API测试</span></h5>
           </div>
-          <div class="table-responsive">
-          <table class="table table-bordered">
-            <thead>
-            <tr>
-                <th>API</th>
-                <th>地址</th>
-                <th>请求方式</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>
-                {{info.var}}
-                </td>
-                <td>
-                {{info.api}}
-                </td>
-                <td>{{info.type}}</td>
-            </tr>
-            <tr>
-                <td colspan="3">
-                    <pre>
-                        {{info.remark}}
-                    </pre>
-                </td>
-            </tr>
-            </tbody>
-          </table>
-          </div>
-          <p>
-              请求参数
-          </p>
-          <div class="table-responsive">
-          <table class="table table-bordered table-striped">
-            <thead>
+          <div id="apiInfomation" v-show="!testStatus">
+            <div class="table-responsive">
+            <table class="table table-bordered">
+              <thead>
               <tr>
-                  <th>参数</th>
-                  <th>类型</th>
-                  <th>默认</th>
-                  <th>范围</th>
-                  <th>说明</th>
+                  <th>API</th>
+                  <th>地址</th>
+                  <th>请求方式</th>
               </tr>
-            </thead>
-            <tbody>
-              <tr v-for="i in info.reqParams">
-                <td>{{i.name}}</td>
-                <td>{{i.type}}</td>
-                <td>{{i.default}}</td>
-                <td>{{i.rang}}</td>
-                <td>{{i.remark}}</td>
+              </thead>
+              <tbody>
+              <tr>
+                  <td>
+                  {{info.var}}
+                  </td>
+                  <td>
+                  {{info.api}}
+                  </td>
+                  <td>{{info.type}}</td>
               </tr>
               <tr>
-                  <td colspan="5">
-                      <pre>{{info.reqDemo}}</pre>
+                  <td colspan="3">
+                      <pre>
+                          {{info.remark}}
+                      </pre>
                   </td>
               </tr>
-            </tbody>
-          </table>
+              </tbody>
+            </table>
+            </div>
+            <p>
+                请求参数
+            </p>
+            <div class="table-responsive">
+            <table class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                    <th>参数</th>
+                    <th>类型</th>
+                    <th>默认</th>
+                    <th>范围</th>
+                    <th>说明</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="i in info.reqParams">
+                  <td>{{i.name}}</td>
+                  <td>{{i.type}}</td>
+                  <td>{{i.default}}</td>
+                  <td>{{i.rang}}</td>
+                  <td>{{i.remark}}</td>
+                </tr>
+                <tr>
+                    <td colspan="5">
+                        <pre>{{info.reqDemo}}</pre>
+                    </td>
+                </tr>
+              </tbody>
+            </table>
+            </div>
+            <p>
+                返回说明
+            </p>
+            <div class="table-responsive">
+            <table class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                    <th>参数</th>
+                    <th>类型</th>
+                    <th>默认</th>
+                    <th>范围</th>
+                    <th>说明</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="i in info.resFields">
+                  <td>{{i.name}}</td>
+                  <td>{{i.type}}</td>
+                  <td>{{i.default}}</td>
+                  <td>{{i.rang}}</td>
+                  <td>{{i.remark}}</td>
+                </tr>
+                <tr>
+                    <td colspan="5">
+                      成功：
+                        <pre>{{info.resDemo.succ}}</pre>
+                      失败：
+                        <pre>{{info.resDemo.err}}</pre>
+                    </td>
+                </tr>
+              </tbody>
+            </table>
+            </div>
           </div>
-          <p>
-              返回说明
-          </p>
-          <div class="table-responsive">
-          <table class="table table-bordered table-striped">
-            <thead>
-              <tr>
-                  <th>参数</th>
-                  <th>类型</th>
-                  <th>默认</th>
-                  <th>范围</th>
-                  <th>说明</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="i in info.resFields">
-                <td>{{i.name}}</td>
-                <td>{{i.type}}</td>
-                <td>{{i.default}}</td>
-                <td>{{i.rang}}</td>
-                <td>{{i.remark}}</td>
-              </tr>
-              <tr>
-                  <td colspan="5">
-                    成功：
-                      <pre>{{info.resDemo.succ}}</pre>
-                    失败：
-                      <pre>{{info.resDemo.err}}</pre>
-                  </td>
-              </tr>
-            </tbody>
-          </table>
+          <div v-show="testStatus">
+            <div class="form-group">
+                <label class="col-sm-3 control-label">API PATH PARAMS：</label>
+                <div class="col-sm-9">
+                  <input type="text" class="form-control" v-model="apiPath">
+                </div>
+              </div>
+            <form id="testForm" name="Login" :method="info.type" :action="apiPath">
+              <div class="form-group" v-for="i in info.reqParams">
+                <label class="col-sm-3 control-label">{{i.name}}：</label>
+                <div class="col-sm-9">
+                  <input type="text" class="form-control" :name="i.name" :value="i.default">
+                </div>
+              </div>
+            </form>
+            <span class="btn btn-primary testBtn" v-on:click="apiSubmit">提交</span>
           </div>
         </div>
   </div>
@@ -107,7 +137,9 @@
 export default {
   data () {
     return {
-      info: {}
+      info: {},
+      apiPath: '',
+      testStatus: false
     }
   },
   computed: {
@@ -136,6 +168,32 @@ export default {
       }, (res) => {
         self.info = {}
       })
+    },
+    apiTest () {
+      this.testStatus = !this.testStatus
+    },
+    apiSubmit () {
+      let i = this.info.api.indexOf(':')
+      let api = ''
+      if (i >= 0) {
+        let _params = this.info.api.substr(i)
+        let _pArr = _params.replace(new RegExp(':', 'g'), '').split('/')
+        let _str = ''
+        for (let x of _pArr) {
+          _str += _str === '' ? this.info.reqDemo[x] : '/' + this.info.reqDemo[x]
+        }
+        api = this.info.api.substr(0, i) + _str
+      } else {
+        api = this.info.api
+      }
+      if (this.apiPath === '') {
+        this.apiPath = api
+      } else {
+        this.apiPath += api
+      }
+      let testForm = document.querySelector('#testForm')
+      testForm.action = this.apiPath
+      testForm.submit()
     }
   }
 }
